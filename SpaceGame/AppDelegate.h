6f2a63f07a187dbb7cd5a659e174026d6ea3f6d0
Karl Andreas <kajohansen.1@gmail.com>
2014-06-03 15:56:10 +0200

    Initial Commit

---
+++ SpaceGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpaceGame
+//
+//  Created by Super User on 03.06.14.
+//  Copyright (c) 2014 KAjohansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpaceGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpaceGame
+//
+
+//  Copyright (c) 2014 KAjohansen. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
